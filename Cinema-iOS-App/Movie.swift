77f659d477a94d0cf02e5d5b6811892489785b43
Chivon <chhaichivon@2ntkh.com>
2017-07-31 05:15:29 +0700

    add alamofire, swiftyJSON and Kingfisher

---
+++ Cinema-iOS-App/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  Cinema-iOS-App
+//
+//  Created by Chhai Chivon on 7/31/17.
+//  Copyright © 2017 Chhai Chivon. All rights reserved.
+//
+
+import Foundation
+
+class Movie{
+    
+}
--- Cinema-iOS-App/ViewController.swift
+++ Cinema-iOS-App/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        Alamofire.request("https://api.themoviedb.org/3/movie/550?api_key=118fbeb9349e606e9e0f67b7def474ac").responseJSON { response in
+            debugPrint(response)
+            
+            if let json = response.result.value {
+                print("JSON: \(json)")
+            }
+        }
+
+    
     }
 
     override func didReceiveMemoryWarning() {
