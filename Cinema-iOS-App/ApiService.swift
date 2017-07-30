95fc1fd0cf029e4cbb8217bd215050ed03bea37b
Chivon <chhaichivon@2ntkh.com>
2017-07-31 05:59:02 +0700

    Add MVC

---
+++ Cinema-iOS-App/ApiService.swift
@@ -0,0 +1,9 @@
+//
+//  ApiService.swift
+//  Cinema-iOS-App
+//
+//  Created by Chhai Chivon on 7/31/17.
+//  Copyright © 2017 Chhai Chivon. All rights reserved.
+//
+
+import Foundation
---
+++ Cinema-iOS-App/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  Cinema-iOS-App
+//
+//  Created by Chhai Chivon on 7/31/17.
+//  Copyright © 2017 Chhai Chivon. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
